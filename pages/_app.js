8c99800e21e2e8c99602f9d5cf8879430f505845
Michael Zhao <michaelzhao314@gmail.com>
2022-09-05 15:20:50 -0500

    Added basic CSS and board components to index

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>My Next App!</title>
+        <title>Wordle!</title>
       </Head>
       <Component {...pageProps} />
     </>
---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <h1>Wordle!</h1>
+      <div
+        style={{
+          height: '30vw',
+          width: '25vw',
+          padding: '1vw',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1vw 0',
+        }}
+      >
+        
+      </div>
+    </div>
+  )
+}
