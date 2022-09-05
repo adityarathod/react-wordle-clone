c3574c723285a9b548f6cadb6625eef2b23fb28c
Michael Zhao <michaelzhao314@gmail.com>
2022-09-05 17:55:27 -0500

    Added word list and answer random generation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect } from 'react'
-import Word from '../components/word'
+import React, { useEffect, useState } from 'react';
+import Word from '../components/word';
+
+import data from '../components/data.json';
 
 export default function Home() {
+  const [answer, setAnswer] = useState(null);
+
+  // On page load, start the game by generating a word
+  useEffect(() => {
+    // Select a random word from the list of answers
+    // and stores it in the answer state variable
+    const randIndex = Math.floor(Math.random() * data.answers.length);
+    const randWord = data.answers[randIndex];
+    console.log("Random word at index ", randIndex, ": ", randWord);
+    setAnswer(randWord);
+  }, []);
+
   // Start listening for events when page loads
   useEffect(() => {
-    window.addEventListener('keydown', handleKey)
+    window.addEventListener('keydown', handleKey);
     return () => {
-      window.removeEventListener('keydown', handleKey)
-    }
-  }, [])
-  
+      window.removeEventListener('keydown', handleKey);
+    };
+  }, []);
+
   // Handle key presses
   const handleKey = (event) => {
     // If a letter is typed
     if (event.keyCode >= 65 && event.keyCode <= 90) {
       console.log(event.key.toLowerCase());
     }
-  }
+  };
 
   return (
     <div
@@ -41,5 +55,5 @@ export default function Home() {
         <Word />
       </div>
     </div>
-  )
+  );
 }
