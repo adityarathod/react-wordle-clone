00cd697effa04827a29ad01e0f6f1c3dc90c47a3
Michael Zhao <michaelzhao314@gmail.com>
2022-09-05 17:56:27 -0500

    Updated formatting and spacing

--- components/word.jsx
+++ components/word.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Character from './character'
+import React from 'react';
+import Character from './character';
 
 export default function Word() {
-  return (
-    <div
-      style={{
-        width: '100%',
-        flexGrow: 1,
-        flexBasis: 0,
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '0 1vw',
-      }}
-    >
-        <Character />
-        <Character />
-        <Character />
-        <Character />
-        <Character />
-    </div>
-  )
+    return (
+        <div
+            style={{
+                width: '100%',
+                flexGrow: 1,
+                flexBasis: 0,
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '0 1vw',
+            }}
+        >
+            <Character />
+            <Character />
+            <Character />
+            <Character />
+            <Character />
+        </div>
+    );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,56 +4,60 @@ import Word from '../components/word';
 import data from '../components/data.json';
 
 export default function Home() {
-  const [answer, setAnswer] = useState(null);
+    const [answer, setAnswer] = useState(null);
 
-  // On page load, start the game by generating a word
-  useEffect(() => {
-    // Select a random word from the list of answers
-    // and stores it in the answer state variable
-    const randIndex = Math.floor(Math.random() * data.answers.length);
-    const randWord = data.answers[randIndex];
-    console.log("Random word at index ", randIndex, ": ", randWord);
-    setAnswer(randWord);
-  }, []);
+    // On page load, start the game by generating a word
+    useEffect(() => {
+        // Select a random word from the list of answers
+        // and stores it in the answer state variable
+        const randIndex = Math.floor(Math.random() * data.answers.length);
+        const randWord = data.answers[randIndex];
+        console.log('Random word at index ', randIndex, ': ', randWord);
+        setAnswer(randWord);
+    }, []);
 
-  // Start listening for events when page loads
-  useEffect(() => {
-    window.addEventListener('keydown', handleKey);
-    return () => {
-      window.removeEventListener('keydown', handleKey);
+    // Start listening for events when page loads
+    useEffect(() => {
+        window.addEventListener('keydown', handleKey);
+        return () => {
+            window.removeEventListener('keydown', handleKey);
+        };
+    }, []);
+
+    // Handle key presses
+    const handleKey = (event) => {
+        // If a letter is typed
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            console.log(event.key.toLowerCase());
+        }
     };
-  }, []);
 
-  // Handle key presses
-  const handleKey = (event) => {
-    // If a letter is typed
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-      console.log(event.key.toLowerCase());
-    }
-  };
-
-  return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
-      <h1>Wordle!</h1>
-      <div
-        style={{
-          height: '30vw',
-          width: '25vw',
-          padding: '1vw',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1vw 0',
-        }}
-      >
-        <Word />
-        <Word />
-        <Word />
-        <Word />
-        <Word />
-        <Word />
-      </div>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <h1>Wordle!</h1>
+            <div
+                style={{
+                    height: '30vw',
+                    width: '25vw',
+                    padding: '1vw',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1vw 0',
+                }}
+            >
+                <Word />
+                <Word />
+                <Word />
+                <Word />
+                <Word />
+                <Word />
+            </div>
+        </div>
+    );
 }
