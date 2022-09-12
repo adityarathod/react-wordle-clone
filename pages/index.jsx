eab703cd99782b4eb9b08ab3f92b65290d70b5a2
Michael Zhao <michaelzhao314@gmail.com>
2022-09-12 18:50:23 -0500

    Added winning conditions

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,8 +7,9 @@ export default function Home() {
     const [answer, setAnswer] = useState(null);
     const [currGuess, setCurrGuess] = useState('');
     const [currIndex, setCurrIndex] = useState(0);
-    const [guessList, setGuessList] = useState(['', '', '', '', '', '']);
-    const [guessResList, setGuessResList] = useState([null, null, null, null, null, null]);
+    const [guessList, setGuessList] = useState(Array(6).fill(''));
+    const [guessResList, setGuessResList] = useState(Array(5).fill(null));
+    const [end, setEnd] = useState(false);
 
     // On page load, start the game by generating a word
     useEffect(() => {
@@ -22,6 +23,10 @@ export default function Home() {
 
     // Start listening for events when page loads
     useEffect(() => {
+        // Don't listen if the game is over
+        if (end) return;
+
+        // Listen for input
         window.addEventListener('keydown', handleKey);
         return () => {
             window.removeEventListener('keydown', handleKey);
@@ -37,7 +42,7 @@ export default function Home() {
 
     // Handle key presses
     const handleKey = (event) => {
-        if (event.repeat) return;
+        if (end) return;
 
         // If a letter is typed
         if (event.keyCode >= 65 && event.keyCode <= 90) {
@@ -61,7 +66,10 @@ export default function Home() {
             }
 
             // Make sure the guess is in the dictionary
-            if (data.dictionary.indexOf(guess) === -1 && data.answers.indexOf(guess) === -1) {
+            if (
+                data.dictionary.indexOf(guess) === -1 &&
+                data.answers.indexOf(guess) === -1
+            ) {
                 alert(currGuess + ' is not a valid word!');
                 return;
             }
@@ -69,26 +77,50 @@ export default function Home() {
             // Check for correct letters
             // -1 = grey, 0 = yellow, 1 = green
             let guessRes = [-1, -1, -1, -1, -1];
-            let answerCheck = answer.split("");
+            let correct = 0;
+            let answerCheck = answer.split('');
+
+            // Loop through the word and find letters that are correct
             for (let i = 0; i < 5; i++) {
                 if (guess.at(i) === answerCheck[i]) {
                     guessRes[i] = 1;
-                    answerCheck[i] = "";
-                }
-            }
-            for (let i = 0; i < 5; i++) {
-                if (guessRes[i] != 1 && answerCheck.indexOf(guess.at(i)) != -1) {
-                    guessRes[i] = 0;
-                    answerCheck[i] = "";
+                    answerCheck[i] = '';
+                    correct++;
                 }
             }
 
-            // Update the state variables
+            // Find letters that are in the word but not in the correct place
+            for (let i = 0; i < 5; i++) {
+                if (
+                    guessRes[i] != 1 &&
+                    answerCheck.indexOf(guess.at(i)) != -1
+                ) {
+                    guessRes[i] = 0;
+                    answerCheck[i] = '';
+                }
+            }
+
+            // Show the correct words by updating the guessResList
             const guessResListCopy = guessResList.slice(0);
             guessResListCopy[currIndex] = guessRes;
             setGuessResList(guessResListCopy);
+
+            // End the game if all correct
+            if (correct === 5) {
+                alert('YOU WIN!!!');
+                setEnd(true);
+            }
+
+            // End the game if no more guesses left
+            if (currIndex === 5) {
+                alert('No more moves :( The word is ' + answer);
+                setEnd(true);
+                return;
+            }
+
+            // Update the counting state variables
             setCurrIndex(currIndex + 1);
-            setCurrGuess("");
+            setCurrGuess('');
         }
     };
 
